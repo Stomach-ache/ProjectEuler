2f5759a818644c1266861da55cb3e819ad2655f1
Stomach-ache <sudaweitong@gmail.com>
2016-09-05 09:57:19 +0800

    How many reversible numbers are there below one-billion

---
+++ p145.cpp
@@ -0,0 +1,80 @@
+/* ***********************************************
+Author        :Stomach_ache
+Created Time  :Mon 05 Sep 2016 09:44:40 AM CST
+File Name     :p145.cpp
+************************************************ */
+
+#include <functional>
+#include <algorithm>
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+#include <sstream>
+#include <numeric>
+#include <utility>
+#include <cassert>
+#include <iomanip>
+#include <vector>
+#include <bitset>
+#include <cstdio>
+#include <string>
+#include <cmath>
+#include <ctime>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <list>
+#include <set>
+#include <map>
+#if __cplusplus > 201103L
+#include <initializer_list>
+#include <unordered_set>
+#include <unordered_map>
+#endif
+
+using namespace std;
+
+#define oo 0x3F3F3F3F
+#define PB push_back
+#define SZ(x) (int)((x).size())
+#define ALL(x) (x).begin(), (x).end()
+#define FOR(i, a, b) for (int _end_ = (b), i = (a); i <= _end_; ++i)
+#define ROF(i, a, b) for (int _end_ = (b), i = (a); i >= _end_; --i)
+
+typedef unsigned int uint;
+typedef long long int64;
+typedef unsigned long long uint64;
+typedef long double real;
+
+
+int64 fpm(int64 b, int64 e, int64 m) { int64 t = 1; for (; e; e >>= 1, b = b * b % m) e & 1 ? t = t * b % m : 0; return t; }
+template<class T> inline bool chkmin(T &a, T b) {return a > b ? a = b, true : false;}
+template<class T> inline bool chkmax(T &a, T b) {return a < b ? a = b, true : false;}
+template<class T> inline T sqr(T x) {return x * x;}
+template <typename T> T gcd(T x, T y) {for (T t; x; ) t = x, x = y % x, y = t; return y; }
+
+int main(void) {
+
+    int N = 1000000000;
+    int ans = 0;
+    for (int i = 12; i < N; ++ i) if (i % 10 != 0) {
+        int j = i, r = 0;
+        while (j > 0) {
+            r = r * 10 + j % 10;
+            j /= 10;
+        }
+        bool flag = true;
+        r = r + i;
+        while (r > 0) {
+            if ((r & 1) == 0) {
+                flag = false; break;
+            }
+            r /= 10;
+        }
+        if (flag) ++ ans;
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
